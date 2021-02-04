9b19bb2b438ac2022ce055222f58db37a8571094
pineda89 <danipinedasanchez@gmail.com>
2021-02-04 18:19:38 +0100

    Added new mouseinput and sendinput format. Thanks to gonutz

--- functions.go
+++ functions.go
@@ -13,7 +13,7 @@ func GetCurrentProcessId() DWORD {
 	return DWORD(id)
 }
 
-func SendInput(inputs ...INPUT) uint32 {
+func SendInput(inputs ...INPUT2) uint32 {
 	if len(inputs) == 0 {
 		return 0
 	}
@@ -23,4 +23,58 @@ func SendInput(inputs ...INPUT) uint32 {
 		unsafe.Sizeof(inputs[0]),
 	)
 	return uint32(ret)
+}
+
+// INPUT is used in SendInput. To create a concrete INPUT type, use the helper
+// functions MouseInput, KeyboardInput and HardwareInput. These are necessary
+// because the C API uses a union here, which Go does not provide.
+type INPUT2 struct {
+	Type uint32
+	// use MOUSEINPUT for the union because it is the largest of all allowed
+	// structures
+	mouse MOUSEINPUT2
+}
+
+type MOUSEINPUT2 struct {
+	Dx        int32
+	Dy        int32
+	MouseData uint32
+	Flags     uint32
+	Time      uint32
+	ExtraInfo uintptr
+}
+
+type KEYBDINPUT2 struct {
+	Vk        uint16
+	Scan      uint16
+	Flags     uint32
+	Time      uint32
+	ExtraInfo uintptr
+}
+
+type HARDWAREINPUT2 struct {
+	Msg    uint32
+	ParamL uint16
+	ParamH uint16
+}
+
+func MouseInput2(input MOUSEINPUT2) INPUT2 {
+	return INPUT2{
+		Type:  INPUT_MOUSE,
+		mouse: input,
+	}
+}
+
+func KeyboardInput2(input KEYBDINPUT2) INPUT2 {
+	return INPUT2{
+		Type:  INPUT_KEYBOARD,
+		mouse: *((*MOUSEINPUT2)(unsafe.Pointer(&input))),
+	}
+}
+
+func HardwareInput2(input HARDWAREINPUT2) INPUT2 {
+	return INPUT2{
+		Type:  INPUT_HARDWARE,
+		mouse: *((*MOUSEINPUT2)(unsafe.Pointer(&input))),
+	}
 }
