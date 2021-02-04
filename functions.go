5502ee9347b6d8c9a9cfa93f6a3ed0c2adac9f2d
pineda89 <danipinedasanchez@gmail.com>
2021-02-04 18:15:23 +0100

    SendInput implemented

--- functions.go
+++ functions.go
@@ -1,10 +1,26 @@
 package w32
 
+import "unsafe"
+
 var (
 	getCurrentProcessId        = kernel32.NewProc("GetCurrentProcessId")
+
+	sendInput                        = moduser32.NewProc("SendInput")
 )
 
 func GetCurrentProcessId() DWORD {
 	id, _, _ := getCurrentProcessId.Call()
 	return DWORD(id)
+}
+
+func SendInput(inputs ...INPUT) uint32 {
+	if len(inputs) == 0 {
+		return 0
+	}
+	ret, _, _ := sendInput.Call(
+		uintptr(len(inputs)),
+		uintptr(unsafe.Pointer(&inputs[0])),
+		unsafe.Sizeof(inputs[0]),
+	)
+	return uint32(ret)
 }
